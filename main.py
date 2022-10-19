96bfe4331a22ecc31437e0d5353c26c34a1743c5
mim1q <mim1q.dev@gmail.com>
2022-10-19 13:46:59 +0200

    Added loading mods from json

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+import json
 
+from mod_data import ModData
+
+
+def load_mods_from_json(filename: str) -> list[ModData]:
+    with open(filename) as f:
+        data = json.load(f)
+        return [ModData(**mod) for mod in data["mods"]]
+
+
+def main():
+    mods = load_mods_from_json("data/mods.json")
+    total_downloads = 0
+    for mod in mods:
+        total_downloads += mod.curseforge_downloads + mod.modrinth_downloads
+
+    print(f"Total downloads: {total_downloads}")
 
 
 if __name__ == "__main__":
