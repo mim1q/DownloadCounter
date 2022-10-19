43359deaa0c7e4e63d0d71f5cabb2fa92d2b10ca
mim1q <mim1q.dev@gmail.com>
2022-10-19 13:46:01 +0200

    Switched to " instead of '

--- downloads.py
+++ downloads.py
@@ -17,10 +17,10 @@ def get_curseforge_downloads(mod_id: str) -> int:
     url = CURSEFORGE_PREFIX + mod_id
 
     # Workaround for CFWidget
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent, }
+    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+    headers = {"User-Agent": user_agent, }
 
-    request = urllib.request.Request(url, None, headers)  # The assembled request
+    request = urllib.request.Request(url, None, headers)
     response = urllib.request.urlopen(request)
     data = json.loads(response.read())
 
